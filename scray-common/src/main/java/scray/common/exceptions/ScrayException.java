38d08167a37ac52ca2d3c37cd37710c5b6fc13a7
Andreas Petter <andreas.petter@gmail.com>
2015-01-14 15:02:13 +0100

    add test dependency to scray-java

--- scray-common/src/main/java/scray/common/exceptions/ScrayException.java
+++ scray-common/src/main/java/scray/common/exceptions/ScrayException.java
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package scray.common.serialization;
+package scray.common.exceptions;
 
 import java.util.UUID;
 import java.io.Serializable;
