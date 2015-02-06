a63743d5aad9d108e064a245a164fa2ff8759269
Johannes Strassner <j.strassner@seeburger.de>
2015-02-06 16:09:52 +0100

    add first monitoring mbean

--- scray-querying/src/main/scala/scray/querying/Registry.scala
+++ scray-querying/src/main/scala/scray/querying/Registry.scala
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package scray.querying
-
+ 
 import scala.collection.mutable.HashMap
 import scray.querying.description.{
   Column,
