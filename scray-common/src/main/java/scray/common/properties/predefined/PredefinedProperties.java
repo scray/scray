85c633004fd383cf9837fc3531573e2788d38646
Christian Zirpins <c.zirpins@seeburger.de>
2015-02-23 17:44:03 +0100

    add some constant properties...

--- scray-common/src/main/java/scray/common/properties/predefined/PredefinedProperties.java
+++ scray-common/src/main/java/scray/common/properties/predefined/PredefinedProperties.java
@@ -19,8 +19,14 @@ public class PredefinedProperties {
 			"CASSANDRA_QUERY_CLUSTER_NAME", "Query Cluster");
 	public final static StringProperty CASSANDRA_INDEX_CLUSTER_NAME = new StringProperty(
 			"CASSANDRA_INDEX_CLUSTER_NAME", "Index Cluster");
-	public final static StringProperty SCRAY_HDFS_USER = new StringProperty(
-			"SCRAY_HDFS_USER", "bd");
 	public final static IntProperty MINUTES_PER_BATCH = new IntProperty(
 			"MINUTES_PER_BATCH", 1);
+
+	/* Following properties used as constants (defaults) */
+
+	public final static StringProperty INDEXING_HDFS_PROPERTIES_KEY = new StringProperty(
+			"INDEXING_HDFS_PROPERTIES_KEY", "indexing.hdfs.properties");
+	public final static StringProperty INDEXING_HDFS_PROPERTIES_PATH = new StringProperty(
+			"INDEXING_HDFS_PROPERTIES_PATH", "/tmp/bdq-indexing.properties");
+
 }
