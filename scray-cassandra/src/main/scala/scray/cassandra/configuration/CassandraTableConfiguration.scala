074a34d67514ae8f69f6bf038569f9056a3565b5
Christian Zirpins <c.zirpins@seeburger.de>
2015-03-24 15:59:24 +0100

    refactor

--- scray-cassandra/src/main/scala/scray/cassandra/configuration/CassandraTableConfiguration.scala
+++ scray-cassandra/src/main/scala/scray/cassandra/configuration/CassandraTableConfiguration.scala
@@ -4,20 +4,24 @@ import com.twitter.storehaus.cassandra.cql.CQLCassandraConfiguration.StoreColumn
 import scray.cassandra.util.CassandraUtils
 import com.twitter.util.Try
 
+object CassandraTableConfiguration {
+
+  /**
+   * property name of the property which holds the parallelization used to read and write the column family
+   */
+  val PARALLELIZATION_TABLE_PROPERTY = "index_parallelization"
+
+}
+
 /**
  * logic to define and process table configurations written into Cassandra column families comments
  */
 class CassandraTableConfiguration {
-  
+
   /**
-   * property name of the property which holds the parallelization used to read and write the column family 
+   * return an Option with the function to get the
    */
-  val PARALLELIZATION_TABLE_PROPERTY = "index_parallelization"
-  
-  /**
-   * return an Option with the function to get the 
-   */
-  def parallelizationFunction(cf: StoreColumnFamily): () => Option[Int] = 
-    () => CassandraUtils.getTablePropertyFromCassandra(cf, PARALLELIZATION_TABLE_PROPERTY).flatMap(prop => Try(prop.toInt).toOption)
+  def parallelizationFunction(cf: StoreColumnFamily): () => Option[Int] =
+    () => CassandraUtils.getTablePropertyFromCassandra(cf, CassandraTableConfiguration.PARALLELIZATION_TABLE_PROPERTY).flatMap(prop => Try(prop.toInt).toOption)
 
 }
