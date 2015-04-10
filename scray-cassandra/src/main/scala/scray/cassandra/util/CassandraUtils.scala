e99c10286f96b50c7d0427a79a8d36ab4f1bd9ac
Christian Zirpins <c.zirpins@seeburger.de>
2015-04-10 16:02:20 +0200

    refactor yaml creation

--- scray-cassandra/src/main/scala/scray/cassandra/util/CassandraUtils.scala
+++ scray-cassandra/src/main/scala/scray/cassandra/util/CassandraUtils.scala
@@ -1,30 +1,30 @@
 package scray.cassandra.util
 
-import com.datastax.driver.core.{KeyspaceMetadata, Metadata, TableMetadata}
+import com.datastax.driver.core.{ KeyspaceMetadata, Metadata, TableMetadata }
 import com.twitter.storehaus.cassandra.cql.CQLCassandraConfiguration.StoreColumnFamily
 import org.yaml.snakeyaml.Yaml
 import com.twitter.util.Try
-import java.util.{Map => JMap, HashMap => JHashMap}
+import java.util.{ Map => JMap, HashMap => JHashMap }
 
 object CassandraUtils {
 
   /**
    * convenience method to retrieve KeyspaceMetadata from a StoreColumnFamily object
    */
-  def getKeyspaceMetadata(cf: StoreColumnFamily): KeyspaceMetadata = 
+  def getKeyspaceMetadata(cf: StoreColumnFamily): KeyspaceMetadata =
     cf.session.getSession.getCluster().getMetadata().getKeyspace(Metadata.quote(cf.session.getKeyspacename))
-   
+
   /**
    * convenience method to retrieve KeyspaceMetadata from a StoreColumnFamily object
    */
   def getTableMetadata(cf: StoreColumnFamily, km: Option[KeyspaceMetadata] = None): TableMetadata = {
     val kspaceMeta = km match {
       case Some(ksm) => ksm
-      case None => getKeyspaceMetadata(cf)
+      case None      => getKeyspaceMetadata(cf)
     }
     kspaceMeta.getTable(cf.getPreparedNamed)
   }
-  
+
   /**
    * *not so fast* and *not thread-safe* method to write a property into the table comment of Cassandra.
    * If s.th. else is in the table comment, it will be overwritten.
@@ -32,24 +32,24 @@ object CassandraUtils {
    * If you need synchronization please use external synchronization, e.g. Zookeeper.
    */
   def writeTablePropertyToCassandra(cf: StoreColumnFamily, property: String, value: String) = {
+    val yaml = createYaml(property, value, getTablePropertiesFromCassandra(cf))
+    val cql = s"ALTER TABLE ${cf.getPreparedNamed} WITH comment='$yaml'"
+    cf.session.getSession.execute(cql)
+  }
+
+  def createYaml(property: String, value: String, currentMap: Option[JMap[String, String]] = None): String = {
     def escapeQuotes(str: String) = str.replaceAll("'", "''")
-    // read
-    val currentMap = getTablePropertiesFromCassandra(cf)
-    // merge
     val map = currentMap match {
-      case Some(properties) => 
+      case Some(properties) =>
         properties.put(property, value)
       case None =>
         val properties = new JHashMap[String, String]
         properties.put(property, value)
         properties
     }
-    val yaml = escapeQuotes(new Yaml().dump(map))
-    // write
-    val cql = s"ALTER TABLE ${cf.getPreparedNamed} WITH comment='$yaml'"
-    cf.session.getSession.execute(cql)
+    escapeQuotes(new Yaml().dump(map))
   }
-  
+
   /**
    * *not so fast* method to read all table properties stored in a comment of a column family from Cassandra.
    * Uses YAML to store properties in a string.
@@ -64,13 +64,13 @@ object CassandraUtils {
         Option(yaml.load(content).asInstanceOf[JMap[String, String]])
       }.toOption.flatten
     }
-  } 
-  
+  }
+
   /**
    * *not so fast* method to read a table property stored in a comment of a column family from Cassandra.
    * Uses YAML to store properties in a string.
    * If you need synchronization to sync with writers please use external synchronization, e.g. Zookeeper.
    */
-  def getTablePropertyFromCassandra(cf: StoreColumnFamily, property: String): Option[String] = 
+  def getTablePropertyFromCassandra(cf: StoreColumnFamily, property: String): Option[String] =
     getTablePropertiesFromCassandra(cf).flatMap(map => Option(map.get(property)))
 }
