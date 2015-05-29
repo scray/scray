5c672ce8391a5adf96ade38f83b219055b8639a1
Andreas Petter <andreas.petter@gmail.com>
2015-05-29 18:08:25 +0200

    make scheme return multiple journal entries

--- scray-storm/src/main/scala/scray/storm/scheme/GeneralJournalScheme.scala
+++ scray-storm/src/main/scala/scray/storm/scheme/GeneralJournalScheme.scala
@@ -3,20 +3,30 @@ package scray.storm.scheme
 import com.twitter.tormenta.scheme.Scheme
 import java.io.ByteArrayInputStream
 import java.io.DataInputStream
+import scala.annotation.tailrec
 
 /**
  * scheme to read a stream of references from Kafka
  */
 class GeneralJournalScheme[T](readReference: DataInputStream => T) extends Scheme[ScrayKafkaJournalEntry[T]] {
 
-   override def decode(bytes: Array[Byte]): TraversableOnce[ScrayKafkaJournalEntry[T]] = {
-     val bais = new ByteArrayInputStream(bytes)
-     val dis = new DataInputStream(bais)
-     try {
-       Seq(ScrayKafkaJournalEntry(dis.readUTF(), readReference(dis), dis.readLong(), dis.readBoolean()))
-     } finally {
-       dis.close()
-     }
-   } 
+  @tailrec def readFullBuffer(bais: ByteArrayInputStream, dis: DataInputStream, 
+                              acc: Seq[ScrayKafkaJournalEntry[T]]): Seq[ScrayKafkaJournalEntry[T]] = {
+    if(bais.available() <= 0) {
+      acc
+    } else {
+      readFullBuffer(bais, dis, acc ++ 
+                     Seq(ScrayKafkaJournalEntry(dis.readUTF(), readReference(dis), dis.readLong(), dis.readBoolean())))
+    }
+  } 
   
+  override def decode(bytes: Array[Byte]): TraversableOnce[ScrayKafkaJournalEntry[T]] = {
+    val bais = new ByteArrayInputStream(bytes)
+    val dis = new DataInputStream(bais)
+    try {
+      readFullBuffer(bais, dis, Seq())
+    } finally {
+      dis.close()
+    }
+  }
 }
