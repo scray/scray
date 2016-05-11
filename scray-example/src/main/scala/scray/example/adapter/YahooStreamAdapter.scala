c983c7c916b22cf177a89258752489657d9040cf
Stefan Obermeier <st.obermeier@seeburger.de>
2016-05-11 18:08:02 +0200

    Add new Yahoo parser.

--- scray-example/src/main/scala/scray/example/adapter/YahooStreamAdapter.scala
+++ scray-example/src/main/scala/scray/example/adapter/YahooStreamAdapter.scala
@@ -1,17 +1,18 @@
 package scray.example.adapter
 
+import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 import java.net.URLConnection
+import java.nio.CharBuffer
 
-import org.apache.http.NameValuePair
+import scala.util.matching.Regex
+import scala.util.parsing.combinator.RegexParsers
+
+import org.apache.http.HttpResponse
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
-import org.apache.http.message.BasicNameValuePair
-import java.io.BufferedReader
-import org.apache.http.HttpResponse
-import scala.util.matching.Regex
 
 class YahooStreamAdapter {
 
@@ -35,59 +36,55 @@ class YahooStreamAdapter {
 
     var line = "";
     val buffer = new Array[Char](10000);
+
+    val parser = new YahooStockStreamParser
+
     while(true) {
       var dataSize = rd.read(buffer)
-      val string =  new String(buffer)
       
-      val pattern = new Regex("""\(.*?\)""")
-      
-      println(string)
-      val fffd = (pattern findAllIn string)
-      fffd.map { x => println("AAAAAAAAAAAAAa" + x) }     
+      val string = CharBuffer.wrap(buffer, 0, dataSize)
+      println(parser.parseAll(parser.start, string))
     }
 
 
   }
+      class YahooStockStreamParser extends RegexParsers {
+        override val skipWhitespace = false
+        override val whiteSpace = """[ \t]""".r
 
-  def pars(c: Char, state: TokenizerState.Value): (TokenizerState.Value, Char) = {
-    import scray.example.adapter.TokenizerState._
-    state match {
-      case WAIT_FOR_OBJECT => {
-        if (c == '(') {
-          (OBJECT_START_1, c)
-        } else {
-          (WAIT_FOR_OBJECT, c)
+        def start: Parser[Option[List[Share]]] = HEADER.? ~ time.? ~ shareObject.* ^^ {
+          case header ~ time ~ shareObject => { Some(shareObject) }
         }
-      }
-      case OBJECT_START_1 => {
-        if (c == '{') {
-          (COLLECT_OBJECT_DATA, c)
-        } else {
-          (WAIT_FOR_OBJECT, c)
-        }
-      }
-      case COLLECT_OBJECT_DATA => {
-        if (c == '}') {
-          (OBJECT_END_1, c)
-        } else {
-          (COLLECT_OBJECT_DATA, c)
-        }
-      }
-      case OBJECT_END_1 => {
-        if (c == '}') {
-          (OBJECT_END_2, c)
-        } else {
-          (COLLECT_OBJECT_DATA, c)
-        }
-      }
-      case OBJECT_END_2 => {
-        (WAIT_FOR_OBJECT, c)
-      }
-    }
-    // val add = "abc<def>ffa".map { x => findStart(x) }.flatten.map { x => println("////////////// \t" + x) }
-  }
-}
 
-object TokenizerState extends Enumeration {
-  val OBJECT_START_1, OBJECT_END_1, OBJECT_END_2, COLLECT_OBJECT_DATA, WAIT_FOR_OBJECT = Value
+        def shareObject = OBJECT_PREFIX ~ STRING ~ ":{" ~
+          ("l10:" <~ STRING).? ~
+          (",a00:" <~ STRING).? ~
+          (",b00:" <~ STRING).? ~
+          (",g00:" <~ STRING).? ~
+          (",h00:" <~ STRING).? ~
+          "}}" ~
+          OBJECT_SUFIX ^^ {
+          case  _ ~ name ~ _ ~ 
+          l10 ~
+          a00 ~
+          b00 ~
+          g00 ~
+          h00 
+          ~ _ ~ _  => {new Share(name,  `=>F`(l10) ,`=>F`(a00), `=>F`(b00), `=>F`(g00), `=>F`(h00))}
+          }
+        
+        def `=>F` (string: Option[String]): Option[Float] = {
+          string.map { x => x.toFloat }
+        }
+        
+        def time = "<script>try{parent.yfs_mktmcb({\"unixtime\":" ~ "[0-9]+".r ~ ",\"open\":" ~ "[0-9]+".r ~ ",\"close\":" ~ "[0-9]+".r ~ "});}catch(e){}</script>"
+
+        lazy val HEADER = "<html><head><script type='text/javascript'> document.domain='finance.yahoo.com'; </script> </head><body></body>"
+        lazy val TIME = "<script>try{parent.yfs_mktmcb({\"unixtime\":1462965657,\"open\":1462973400,\"close\":1462996800});}catch(e){}</script>"
+        lazy val OBJECT_PREFIX = "<script>try{parent.yfs_u1f({"
+        lazy val OBJECT_SUFIX = ");}catch(e){}</script>"
+        lazy val STRING: Parser[String] = "\"" ~> ("\"\"" | "[^\"]".r).* <~ "\"" ^^ {
+          case string => string mkString ("") replaceAll ("\"\"", "\"")
+        }
+      }
 }
--- scray-example/src/test/scala/scray/example/adapter/Adapter.scala
+++ scray-example/src/test/scala/scray/example/adapter/Adapter.scala
@@ -9,102 +9,106 @@ import scala.util.matching.Regex
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.core.JsonParser
 
-
-  @RunWith(classOf[JUnitRunner])
+@RunWith(classOf[JUnitRunner])
 class Adapter extends WordSpec with BeforeAndAfter with BeforeAndAfterAll {
-  import TokenizerState._
-
   "OnlineBatchSync " should {
-//    " detect start " in {
-//      val adapter = new YahooStreamAdapter
-//      
-//      // Detect start
-//      assert(adapter.pars('a', TokenizerState.WAIT_FOR_OBJECT) === WAIT_FOR_OBJECT)
-//      assert(adapter.pars('(', TokenizerState.WAIT_FOR_OBJECT) === OBJECT_START_1)
-//      assert(adapter.pars('{', TokenizerState.OBJECT_START_1) === COLLECT_OBJECT_DATA)
-//    }
-//    " detect end " in {
-//      val adapter = new YahooStreamAdapter
-//      
-//      // Collect end
-//      assert(adapter.pars('}', TokenizerState.COLLECT_OBJECT_DATA) === OBJECT_END_1)
-//      assert(adapter.pars('}', TokenizerState.OBJECT_END_1) === OBJECT_END_2)
-//      assert(adapter.pars('}', TokenizerState.OBJECT_END_2) === WAIT_FOR_OBJECT)
-//    }
-//    " pars string " in {
-//      val adapter = new YahooStreamAdapter
-//      
-//      val x: Stream[Character] = Array[Character]('a', 'b', 'c', '(', '{', 'a').seq.view.toStream
-//      val y = x.scanLeft((WAIT_FOR_OBJECT, 'a'))((state, char) => adapter.pars(char, state._1)).filter { x => x._1 == TokenizerState.COLLECT_OBJECT_DATA}
-//      
-//      y.scanLeft("")((acc, char) => acc + char).f
+    //    " detect start " in {
+    //      val adapter = new YahooStreamAdapter
+    //      
+    //      // Detect start
+    //      assert(adapter.pars('a', TokenizerState.WAIT_FOR_OBJECT) === WAIT_FOR_OBJECT)
+    //      assert(adapter.pars('(', TokenizerState.WAIT_FOR_OBJECT) === OBJECT_START_1)
+    //      assert(adapter.pars('{', TokenizerState.OBJECT_START_1) === COLLECT_OBJECT_DATA)
+    //    }
+    //    " detect end " in {
+    //      val adapter = new YahooStreamAdapter
+    //      
+    //      // Collect end
+    //      assert(adapter.pars('}', TokenizerState.COLLECT_OBJECT_DATA) === OBJECT_END_1)
+    //      assert(adapter.pars('}', TokenizerState.OBJECT_END_1) === OBJECT_END_2)
+    //      assert(adapter.pars('}', TokenizerState.OBJECT_END_2) === WAIT_FOR_OBJECT)
+    //    }
+    //    " pars string " in {
+    //      val adapter = new YahooStreamAdapter
+    //      
+    //      val x: Stream[Character] = Array[Character]('a', 'b', 'c', '(', '{', 'a').seq.view.toStream
+    //      val y = x.scanLeft((WAIT_FOR_OBJECT, 'a'))((state, char) => adapter.pars(char, state._1)).filter { x => x._1 == TokenizerState.COLLECT_OBJECT_DATA}
+    //      
+    //      y.scanLeft("")((acc, char) => acc + char).f
 
-      
-      
-      
-//      val dddd = "abcdefg".foldLeft(WAIT_FOR_OBJECT)((state, char) => adapter.pars(char, state))
-//      
-//      var state = WAIT_FOR_OBJECT
-//        adapter.getStream.map(WAIT_FOR_OBJECT)((state, char) => adapter.pars(char, state)).filter(dd).map
-//      
-//      println(dddd)
-//    }
-    
-    
-//    " throw exception if job already exists" in {
-//     val stream = new YahooStreamAdapter
-//     val sb = new StringBuffer
-//     val buffer = new Array[Char](10000);
-//     
-////     while(true) {
-//
-//       val sb = new StringBuffer
-//       //val len = stream.getStream2
-//
-//       //println("\n\n" + sb.toString() + "\n\n")
-//       //println(sb.toString().split("\\(\\{").foreach { x => x.split("\\}\\}")})
-//       //println(sb.toString().split("\\(\\{").foreach { x => println(x)})
-//
-////       println(sb.toString().
-////           replace("<html><head><script type='text/javascript'> document.domain='finance.yahoo.com'; </script> </head><body></body><script>try{parent.yfs_mktmcb(", "").
-////           replace(");}catch(e){}</script><script>try{parent.yfs_u1f(", "").
-////           replace(";}catch(e){}</script><script>try{parent.yfs_u1f({\"USDUSD=X\":{l10:\"1.0000\",a00:\"1.0000\",b00:\"1.0000\",g00:\"1.0000\",h00:\"1.0000\"}});}catch(e){}</script>", "").
-////           replace(";}catch(e){}</script>", "").
-////           replace("}}", ""))
-//    
-//       
-//       //Thread.sleep(1000)
-// //    }
-//     //buffer.filter { x => ??? }foldLeft(0)((acc, n) => acc)
-//// 
-//     
-//    }
-    
+    //      val dddd = "abcdefg".foldLeft(WAIT_FOR_OBJECT)((state, char) => adapter.pars(char, state))
+    //      
+    //      var state = WAIT_FOR_OBJECT
+    //        adapter.getStream.map(WAIT_FOR_OBJECT)((state, char) => adapter.pars(char, state)).filter(dd).map
+    //      
+    //      println(dddd)
+    //    }
 
-    "extract json object" in {
-      val string = "<script>try{parent.yfs_u1f({\"EOAN.F\":{l10:\"8.45\",a00:\"8.50\",b00:\"8.50\",g00:\"8.41\",h00:\"8.51\"}});}catch(e){}</script>ipt>"
-      
-      val pattern = new Regex("""\(\{.*?\)""")
-      
-      for(stockString <- pattern.findAllIn(string)) {
-       assert(stockString.replaceAll("\\(", "").replace(")", "") == "{\"EOAN.F\":{l10:\"8.45\",a00:\"8.50\",b00:\"8.50\",g00:\"8.41\",h00:\"8.51\"}}")
-      }
+    " throw exception if job already exists" in {
+      val stream = new YahooStreamAdapter
+      val sb = new StringBuffer
+      val buffer = new Array[Char](10000);
+
+      val len = stream.getStream2
+
+      //println("\n\n" + sb.toString() + "\n\n")
+      //println(sb.toString().split("\\(\\{").foreach { x => x.split("\\}\\}")})
+      //println(sb.toString().split("\\(\\{").foreach { x => println(x)})
+
+      //       println(sb.toString().
+      //           replace("<html><head><script type='text/javascript'> document.domain='finance.yahoo.com'; </script> </head><body></body><script>try{parent.yfs_mktmcb(", "").
+      //           replace(");}catch(e){}</script><script>try{parent.yfs_u1f(", "").
+      //           replace(";}catch(e){}</script><script>try{parent.yfs_u1f({\"USDUSD=X\":{l10:\"1.0000\",a00:\"1.0000\",b00:\"1.0000\",g00:\"1.0000\",h00:\"1.0000\"}});}catch(e){}</script>", "").
+      //           replace(";}catch(e){}</script>", "").
+      //           replace("}}", ""))
+
+      //Thread.sleep(1000)
+      //     //buffer.filter { x => ??? }foldLeft(0)((acc, n) => acc)
+      //// 
+      //     
     }
-    "parse json string" in {
-      val jsonObject = "{\"EOAN.F\":{l10:\"8.45\",a00:\"8.50\",b00:\"8.50\",g00:\"8.41\",h00:\"8.51\"}}"
-      import scala.collection.JavaConversions._
-      
-      val mapper = new ObjectMapper()
-      mapper.registerModule(DefaultScalaModule)
-      
-      import scray.example.adapter.Share
-      val user = mapper.readValue(jsonObject, classOf[Share])
-      
-      val pattern = new Regex("""\(.*?\)""")
-      for(stockString <- pattern.findAllIn(jsonObject)) {  
-       println(stockString.replaceAll("\\(", "").replace(")", ""))
-      }
-    }
+
+    //    "extract json object" in {
+    //      val string = "<script>try{parent.yfs_u1f(x);}catch(e){}</script>ipt>"
+    //      
+    //      val pattern = new Regex("""\(\{.*?\)""")
+    //      
+    //      for(stockString <- pattern.findAllIn(string)) {
+    //       assert(stockString.replaceAll("\\(", "").replace(")", "") == "{\"EOAN.F\":{l10:\"8.45\",a00:\"8.50\",b00:\"8.50\",g00:\"8.41\",h00:\"8.51\"}}")
+    //      }
+    //    }
+    //    "parse json string" in {
+    //      val jsonObject = "{\"EOAN.F\":{\"l10\":\"8.45\",a00:\"8.50\",b00:\"8.50\",g00:\"8.41\",h00:\"8.51\"}}"
+    //      import scala.collection.JavaConversions._
+    //      
+    //      val mapper = new ObjectMapper()
+    //      mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+    //      mapper.registerModule(DefaultScalaModule)
+    //      
+    //      import scray.example.adapter.Share
+    //      val user = mapper.readValue(jsonObject, classOf[Share])
+    //      
+    //      val pattern = new Regex("""\(.*?\)""")
+    //      for(stockString <- pattern.findAllIn(jsonObject)) {  
+    //       println(stockString.replaceAll("\\(", "").replace(")", ""))
+    //      }
+    //    }
+
+//    "pars example" in {
+//      import util.parsing.combinator.RegexParsers
+//
+//      val header = "<html><head><script type='text/javascript'> document.domain='finance.yahoo.com'; </script> </head><body></body>"
+//
+//      val line1 = "<script>try{parent.yfs_mktmcb({\"unixtime\":1462965657,\"open\":1462973400,\"close\":1462996800});}catch(e){}</script><script>try{parent.yfs_u1f({\"BAS.F\":{l10:\"68.48\",a00:\"68.53\",b00:\"68.51\",g00:\"68.32\",h00:\"69.20\"}});}catch(e){}</script><script>try{parent.yfs_u1f({\"DBK.F\":{l10:\"14.58\",a00:\"14.67\",b00:\"14.65\",g00:\"14.56\",h00:\"15.04\"}});}catch(e){}</script>"
+//      val line2 = "<script>try{parent.yfs_u1f({\"SAP.F\":{l10:\"68.44\",a00:\"68.33\",b00:\"68.31\",g00:\"68.28\",h00:\"68.93\"}});}catch(e){}</script><script>try{parent.yfs_u1f({\"EOAN.F\":{l10:\"8.14\",a00:\"8.13\",b00:\"8.13\",g00:\"8.06\",h00:\"8.69\"}});}catch(e){}</script><script>try{parent.yfs_u1f({\"ADS.F\":{l10:\"113.36\",a00:\"113.45\",b00:\"113.40\",g00:\"113.20\",h00:\"114.74\"}});}catch(e){}</script>"
+//
+//     
+//
+//      val parser = new YahooStockStreamParser
+//      println(parser.parseAll(parser.start, header + line1 + line2))
+//
+//    }
   }
 }
