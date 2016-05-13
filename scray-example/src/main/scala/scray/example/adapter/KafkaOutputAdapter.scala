3332815c53fdb52ed29176d616e6c52448e9d0ad
Stefan Obermeier <st.obermeier@seeburger.de>
2016-05-13 11:53:05 +0200

    Add streaming project.

--- scray-example/src/main/scala/scray/example/adapter/KafkaOutputAdapter.scala
+++ scray-example/src/main/scala/scray/example/adapter/KafkaOutputAdapter.scala
@@ -1,12 +1,41 @@
 package scray.example.adapter
 
 import java.util.concurrent.BlockingQueue
+import kafka.javaapi.producer.Producer
+import kafka.producer.KeyedMessage
+import kafka.producer.ProducerConfig
+import java.util.Properties
+import scala.util.Random
+import java.util.Date
+import kafka.utils.VerifiableProperties
+import kafka.serializer.Encoder
+import kafka.utils.VerifiableProperties
+
 
 class KafkaOutputAdapter(val inputQueue: BlockingQueue[Share]) extends Thread {
   
   override def run() {
-    while(true) {
-      println(inputQueue.take())
-    }
+        val rnd = new Random();
+ 
+        val props = new Properties();
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("serializer.class", "com.seeburger.bdq.spark.GenericKafkaKryoSerializer");
+        props.put("partitioner.class", "scray.example.adapter.SimplePartitioner");
+        props.put("request.required.acks", "1");
+ 
+        val config = new ProducerConfig(props);
+ 
+        val producer = new Producer[String, Share](config)
+ 
+        for(events <- 1 until 100) { 
+               val message = new KeyedMessage[String, Share]("test", events.toString(), inputQueue.take());
+               println(message)
+               producer.send(message);
+        }
+        producer.close;
+    
+//    while(true) {
+//      println(inputQueue.take())
+//    }
   }
 }
---
+++ scray-streaming/src/main/scala/com/seeburger/bdq/spark/serializers/GenericKafkaKryoSerializer.scala
@@ -0,0 +1,36 @@
+package com.seeburger.bdq.spark.serializers
+
+import com.esotericsoftware.kryo.io.Input
+import kafka.serializer.Decoder
+import kafka.serializer.Encoder
+import java.io.ByteArrayInputStream
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.Output
+import java.io.ByteArrayOutputStream
+
+/**
+ * A generic kryo serializer to be used with Kafka
+ * It is somewhat expensive in the sense that it always writes the class information.
+ * However, it is generic and can easily be used without writing special serializers.
+ * @author: Andreas Petter <a.petter@seeburger.de>
+ */
+class GenericKafkaKryoSerializer[T <: Any](registerScalaCollections: Boolean = true) extends Decoder[T] with Encoder[T] {
+  
+  val kryo = new Kryo()
+  if(registerScalaCollections) {
+    new AllScalaRegistrar()(kryo)
+  }
+  
+  override def fromBytes(bytes: Array[Byte]): T = {
+    val input = new Input(new ByteArrayInputStream(bytes))
+    kryo.readClassAndObject(input).asInstanceOf[T]
+  }
+  
+  override def toBytes(that: T): Array[Byte] = {
+    val output = new Output(new ByteArrayOutputStream())
+		kryo.writeClassAndObject(output, that)
+		val result = output.getBuffer()
+		output.close();
+		result
+  }
+}
