3e9b512f06acfdf4fc102934dc6dee49a5f91d0a
Stefan Obermeier <st.obermeier@seeburger.de>
2016-06-01 12:30:43 +0200

    Clean up.

--- scray-example/src/main/scala/scray/example/adapter/YahooStreamAdapter.scala
+++ scray-example/src/main/scala/scray/example/adapter/YahooStreamAdapter.scala
@@ -18,10 +18,10 @@ import java.util.concurrent.LinkedBlockingQueue
 import scala.util.parsing.combinator._
 import java.util.concurrent.BlockingQueue
 
-
-
 class YahooStreamAdapter(val outputQueue: BlockingQueue[Share]) extends Thread with LazyLogging {
-  val url = "http://streamerapi.finance.yahoo.com/streamer/1.0?s=%5EGDAXI,USD=X&o=DBK.F,ADS.F,BAS.F,EOAN.F,SAP.F&k=l10,a00,b00,g00,h00&j=l10,a00,b00,g00,h00&r=0&marketid=us_market&callback=parent.yfs_u1f&mktmcb=parent.yfs_mktmcb&gencallback=parent.yfs_gencb";
+  val url = "http://streamerapi.finance.yahoo.com/streamer/1.0?s=%5EGDAXI,USD=X&o=" + 
+    "DBK.F,ADS.F,BAS.F,EOAN.F,SAP.F&k=l10,a00,b00,g00,h00&j=l10,a00,b00,g00,h00" + 
+    "&r=0&marketid=us_market&callback=parent.yfs_u1f&mktmcb=parent.yfs_mktmcb&gencallback=parent.yfs_gencb";
 
   override def run {
     val client: HttpClient = new DefaultHttpClient();
