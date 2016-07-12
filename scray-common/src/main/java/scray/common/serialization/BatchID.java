e988a9c7967ad692788e24071f5d35cc3adc7d43
Stefan Obermeier <st.obermeier@seeburger.de>
2016-07-12 13:03:02 +0200

    Add BatchID

---
+++ scray-common/src/main/java/scray/common/serialization/BatchID.java
@@ -0,0 +1,34 @@
+package scray.common.serialization;
+
+import java.io.Serializable;
+
+/**
+ * new BatchID for use with sync/api
+ * @author stefan
+ *
+ */
+public class BatchID implements Serializable {
+
+	public BatchID(long startTime, long endTime) {
+		batchStart = startTime;
+		batchEnd = endTime;
+	}
+	
+	private static final long serialVersionUID = -7082915998944716142L;
+
+	private long batchStart;
+	private long batchEnd;
+
+	public long getBatchStart() {
+		return batchStart;
+	}
+
+	public long getBatchEnd() {
+		return batchEnd;
+	}
+
+	@Override
+	public String toString() {
+		return "BatchID " + batchStart + ":" + batchEnd;
+	}
+}
