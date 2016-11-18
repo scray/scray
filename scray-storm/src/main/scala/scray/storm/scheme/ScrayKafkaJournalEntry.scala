d29aacbe46f38bd91e098d8d9b4455de72bdcd14
Stefan Obermeier <st.obermeier@seeburger.de>
2016-11-18 18:00:59 +0100

    Remove scray-storm

--- scray-storm/src/main/scala/scray/storm/scheme/ScrayKafkaJournalEntry.scala
+++
@@ -1,6 +0,0 @@
-package scray.storm.scheme
-
-/**
- * Abstract journal entry which reflects a reference into the original data set
- */
-case class ScrayKafkaJournalEntry[T](tableName: String, reference: T, time: Long, delete: Boolean)
