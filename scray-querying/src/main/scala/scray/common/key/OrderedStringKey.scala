060c6f20e7c1d8d3e120e86d952879592a23aeee
Stefan Obermeier <st.obermeier@seeburger.de>
2017-02-09 19:57:08 +0100

    Add more orderd key tests

--- scray-querying/src/main/scala/scray/common/key/OrderedStringKey.scala
+++ scray-querying/src/main/scala/scray/common/key/OrderedStringKey.scala
@@ -5,17 +5,26 @@ import scala.util.Sorting
 class OrderedStringKey(value: Array[String]) extends ScrayKey(value) {
   
   Sorting.quickSort(value)
-  val key = value.foldLeft("")((acc, element) => acc + "_" + element)
+  val key = value.foldLeft("")((acc, element) => { 
+      if(acc.size < 1) {
+        element
+      } else {
+        acc + "_" + element
+      }
+    }
+  )
   
   def getKeyAsString: String = {
     key
   }
+  
   override def equals(other: Any): Boolean = {
-    false
+    // FIXME
+    true
   }
   
   override def hashCode: Int = {
-    1
+    key.hashCode()
   }
   
 }
--- scray-querying/src/test/scala/scray/common/key/TestOrderedStringKey.scala
+++ scray-querying/src/test/scala/scray/common/key/TestOrderedStringKey.scala
@@ -8,12 +8,40 @@ import scray.querying.sync.SyncTableBasicClasses.SyncTableRowEmpty
 @RunWith(classOf[JUnitRunner])
 class TestOrderedStringKey extends WordSpec {
   "OrderedStringKey " should {
-    " generate key from arry of strings" in {
+    " generate key from array of strings" in {
       
       val keyElements = Array("d", "a", "c", "b")
       val key = new OrderedStringKey(keyElements)
       
       assert(key.getKeyAsString == "a_b_c_d")
+    }
+    " get hash code for different keys" in {
+
+      val key1 = new OrderedStringKey(Array("d", "a", "c", "b"))
+      val key2 = new OrderedStringKey(Array("d1", "a1", "c1", "b1"))
+
+      assert(key1.hashCode() == -1293253427)
+      assert(key2.hashCode() == -1106818755)
+
+    }
+    " ignore input ordering " in {
+      val expectedKeyAsString = "a_b_c_d"
+      
+      val keyV1 = new OrderedStringKey(Array("a", "b", "c", "d"))
+      val keyV2 = new OrderedStringKey(Array("b", "c", "d", "a"))
+      val keyV3 = new OrderedStringKey(Array("c", "d", "a", "b"))
+      val keyV4 = new OrderedStringKey(Array("d", "a", "b", "c"))
+
+      assert(keyV1.getKeyAsString == expectedKeyAsString)
+      assert(keyV2.getKeyAsString == expectedKeyAsString)
+      assert(keyV3.getKeyAsString == expectedKeyAsString)
+      assert(keyV4.getKeyAsString == expectedKeyAsString)
+
+      
+      assert(keyV1.hashCode() == -1293253427)
+      assert(keyV2.hashCode() == -1293253427)
+      assert(keyV3.hashCode() == -1293253427)
+      assert(keyV4.hashCode() == -1293253427)
     }
   }  
 }
