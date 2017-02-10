09aaf6c182a660290852530e05274ea87a33b116
Stefan Obermeier <st.obermeier@seeburger.de>
2017-02-10 08:55:46 +0100

    Implement equals method

--- scray-querying/src/main/scala/scray/common/key/OrderedStringKey.scala
+++ scray-querying/src/main/scala/scray/common/key/OrderedStringKey.scala
@@ -3,28 +3,41 @@ package scray.common.key
 import scala.util.Sorting
 
 class OrderedStringKey(value: Array[String]) extends ScrayKey(value) {
-  
+
   Sorting.quickSort(value)
-  val key = value.foldLeft("")((acc, element) => { 
-      if(acc.size < 1) {
-        element
-      } else {
-        acc + "_" + element
-      }
+  val key = value.foldLeft("")((acc, element) => {
+    if (acc.size < 1) {
+      element
+    } else {
+      acc + "_" + element
     }
-  )
-  
+  })
+
   def getKeyAsString: String = {
     key
   }
-  
+
   override def equals(other: Any): Boolean = {
-    // FIXME
-    true
+    if (other == null) {
+      false
+    }
+
+    if (other == this) {
+      true
+    }
+
+    if (!(other.isInstanceOf[OrderedStringKey])) {
+      false
+    }
+
+    // other is a OrderedStringKey
+    val otherKey = other.asInstanceOf[OrderedStringKey]
+
+    return this.getKeyAsString == otherKey.getKeyAsString
   }
-  
+
   override def hashCode: Int = {
     key.hashCode()
   }
-  
+
 }
