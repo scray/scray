28a6746ed1470b908c9a4aa500ddc8c803ddaf0c
Stefan Obermeier <st.obermeier@seeburger.de>
2017-02-10 09:44:17 +0100

    Add some comments

--- scray-querying/src/main/scala/scray/common/key/ScrayKey.scala
+++ scray-querying/src/main/scala/scray/common/key/ScrayKey.scala
@@ -1,10 +1,29 @@
 package scray.common.key
 
-
+/**
+ * Some data produced by an aggregation job are identified by key.
+ * To query this data the scray service requires this key. 
+ * This interface can help to use the same keygeneration class.
+ */
 abstract class ScrayKey[T](value: T) {
 
+  /**
+   * Provide a string representation of the key.
+   * Is used by scray service to query the data.
+   */
   def getKeyAsString: String
+  
 
+  /**
+   * Check if two objects are equals.
+   * Is used by Spark
+   */
   override def equals(other: Any): Boolean
+  
+  
+  /**
+   * Provides a hash code for this class. 
+   * Is used by Spark
+   */
   override def hashCode: Int
 }
