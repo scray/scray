2e842b0a87cd958e83a4cadddc525ad9847cd7b4
Stefan Obermeier <st.obermeier@seeburger.de>
2017-03-29 15:04:09 +0200

    Remove shutdown after exception.

--- scray-loader/src/main/scala/scray/loader/service/RefreshServing.scala
+++ scray-loader/src/main/scala/scray/loader/service/RefreshServing.scala
@@ -109,7 +109,6 @@ class RefreshServing extends LazyLogging {
         }
       } catch {
         case ex: Exception =>
-          client.map { _.shutdown(None) }
           client = None
           getClient(seedAddr)
           if (time < 4) {
