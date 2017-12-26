a9bd016a63e854efd1660846f5e22f8fcf394951
Stefan Obermeier <mail@stefan-obermeier.de>
2017-12-26 21:46:15 +0100

    Switch next version TableIdentifier example

---
+++ scray-jdbc/src/main/scala/scray/jdbc/example/TiVersioningMain.scala
@@ -0,0 +1,26 @@
+package scray.jdbc.example
+
+import scray.jdbc.extractors.ScraySQLDialect
+import scray.jdbc.sync.OnlineBatchSyncJDBC
+import scray.jdbc.sync.JDBCDbSession
+import scray.querying.sync.JobInfo
+import scray.jdbc.sync.JDBCJobInfo
+
+object TiVersioningMain {
+  
+  def main(args: Array[String]) {
+
+    val connection = new JDBCDbSession("jdbc:mariadb://localhost:3306/scray", "scray", "scray")      
+    val syncApi = new OnlineBatchSyncJDBC(connection)  
+    val jobInfo = new JDBCJobInfo("job1")
+    
+    syncApi.initJob(jobInfo)
+    
+    syncApi.startNextBatchJob(jobInfo)
+  
+    syncApi.completeBatchJob(jobInfo)
+    
+    println(syncApi.getQueryableBatchData(jobInfo))
+
+  }
+}
