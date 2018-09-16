00c5c173cc5d51971aab7edb26a17651930b0a6b
Stefan Obermeier <git@stefan-obermeier.de>
2018-09-16 23:30:39 +0200

    Add TextText mapping

---
+++ scray-hdfs/modules/scray-hdfs-writer/src/main/scala/scray/hdfs/io/index/format/sequence/mapping/impl/OutputTextText.scala
@@ -0,0 +1,31 @@
+package scray.hdfs.io.index.format.sequence.mapping.impl
+
+import org.apache.hadoop.io.Text
+
+import scray.hdfs.io.index.format.sequence.mapping.SequenceKeyValuePair
+
+class OutputTextText extends SequenceKeyValuePair[Text, Text, Text, Text] {
+    def getIdxKey(id: String): Text = {
+    new Text(s"{id: ${id}}")
+  }
+  
+  def getIdxValue(id: String, blobSplits: Int, splitSize: Int, updateTime: Long, dataLength: Long): Text = {
+    new Text(s"{id: $id, blobSplits: ${blobSplits}, splitSize: ${splitSize}, updateTime: ${updateTime}, dataLength: ${dataLength}}")  
+  }
+  
+  def getIdxValue(id: String, updateTime: Long, dataLength: Long): Text = {
+    this.getIdxValue(id, 1, 8192, updateTime, dataLength)
+  }
+  
+  def getDataKey(id: String, blobCount: Int = 0): Text = {
+    new Text(s"{id: ${id}, blobCount: ${blobCount}}")
+  }
+  
+  def getDataValue(data: Array[Byte], length: Int) = {
+    new Text(new String(data, length))
+  }
+  
+  def getDataValue(data: Array[Byte]) = {
+    new Text(new String(data))
+  }
+}
