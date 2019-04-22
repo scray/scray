ff5f894da202f39c141eb0c77f825e3f2a9b9f4f
Stefan Obermeier <scray@stefan-obermeier.de>
2019-04-22 19:49:21 +0200

    First try of sync rest api

--- scray-querying/modules/scray-sync-service-rest/src/main/java/io/swagger/sample/data/PetData.java
+++ scray-querying/modules/scray-sync-service-rest/src/main/java/io/swagger/sample/data/PetData.java
@@ -17,86 +17,27 @@
 package io.swagger.sample.data;
 
 import io.swagger.sample.model.Category;
-import io.swagger.sample.model.Pet;
+import io.swagger.sample.model.SyncObject;
 import io.swagger.sample.model.Tag;
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class PetData {
-  static List<Pet> pets = new ArrayList<Pet>();
+  static List<SyncObject> pets = new ArrayList<SyncObject>();
   static List<Category> categories = new ArrayList<Category>();
 
-  static {
-    categories.add(createCategory(1, "Dogs"));
-    categories.add(createCategory(2, "Cats"));
-    categories.add(createCategory(3, "Rabbits"));
-    categories.add(createCategory(4, "Lions"));
-
-    pets.add(createPet(1, categories.get(1), "Cat 1", new String[] {
-        "url1", "url2" }, new String[] { "tag1", "tag2" }, "available"));
-    pets.add(createPet(2, categories.get(1), "Cat 2", new String[] {
-        "url1", "url2" }, new String[] { "tag2", "tag3" }, "available"));
-    pets.add(createPet(3, categories.get(1), "Cat 3", new String[] {
-        "url1", "url2" }, new String[] { "tag3", "tag4" }, "pending"));
-
-    pets.add(createPet(4, categories.get(0), "Dog 1", new String[] {
-        "url1", "url2" }, new String[] { "tag1", "tag2" }, "available"));
-    pets.add(createPet(5, categories.get(0), "Dog 2", new String[] {
-        "url1", "url2" }, new String[] { "tag2", "tag3" }, "sold"));
-    pets.add(createPet(6, categories.get(0), "Dog 3", new String[] {
-        "url1", "url2" }, new String[] { "tag3", "tag4" }, "pending"));
-
-    pets.add(createPet(7, categories.get(3), "Lion 1", new String[] {
-        "url1", "url2" }, new String[] { "tag1", "tag2" }, "available"));
-    pets.add(createPet(8, categories.get(3), "Lion 2", new String[] {
-        "url1", "url2" }, new String[] { "tag2", "tag3" }, "available"));
-    pets.add(createPet(9, categories.get(3), "Lion 3", new String[] {
-        "url1", "url2" }, new String[] { "tag3", "tag4" }, "available"));
-
-    pets.add(createPet(10, categories.get(2), "Rabbit 1", new String[] {
-        "url1", "url2" }, new String[] { "tag3", "tag4" }, "available"));
-  }
-
-  public Pet getPetById(long petId) {
-    for (Pet pet : pets) {
-      if (pet.getId() == petId) {
+  public SyncObject getPetById(String petId) {
+    for (SyncObject pet : pets) {
+    	System.out.println("Process sync point " + pet.getId() + "search " + petId);
+      if (pet.getId().equals(petId)) {
         return pet;
       }
     }
     return null;
   }
 
-  public List<Pet> findPetByStatus(String status) {
-    String[] statues = status.split(",");
-    List<Pet> result = new java.util.ArrayList<Pet>();
-    for (Pet pet : pets) {
-      for (String s : statues) {
-        if (s.equals(pet.getStatus())) {
-          result.add(pet);
-        }
-      }
-    }
-    return result;
-  }
-
-  public List<Pet> findPetByTags(String tags) {
-    String[] tagList = tags.split(",");
-    List<Pet> result = new java.util.ArrayList<Pet>();
-    for (Pet pet : pets) {
-      if (null != pet.getTags()) {
-        for (Tag tag : pet.getTags()) {
-          for (String tagListString : tagList) {
-            if (tagListString.equals(tag.getName()))
-              result.add(pet);
-          }
-        }
-      }
-    }
-    return result;
-  }
-
-  public void addPet(Pet pet) {
+  public void addPet(SyncObject pet) {
     if (pets.size() > 0) {
       for (int i = pets.size() - 1; i >= 0; i--) {
         if (pets.get(i).getId() == pet.getId()) {
@@ -104,42 +45,16 @@ public class PetData {
         }
       }
     }
+    
+    System.out.println("Add sync point" + pet);
     pets.add(pet);
   }
 
-  static Pet createPet(long id, Category cat, String name, String[] urls,
-      String[] tags, String status) {
-    Pet pet = new Pet();
+  static SyncObject createPet(String id, String syncPoint, int version) {
+    SyncObject pet = new SyncObject();
     pet.setId(id);
-    pet.setCategory(cat);
-    pet.setName(name);
-    if (null != urls) {
-      List<String> urlObjs = new ArrayList<String>();
-      for (String urlString : urls) {
-        urlObjs.add(urlString);
-      }
-      pet.setPhotoUrls(urlObjs);
-    }
-    List<Tag> tagObjs = new java.util.ArrayList<Tag>();
-    int i = 0;
-    if (null != tags) {
-      for (String tagString : tags) {
-        i = i + 1;
-        Tag tag = new Tag();
-        tag.setId(i);
-        tag.setName(tagString);
-        tagObjs.add(tag);
-      }
-    }
-    pet.setTags(tagObjs);
-    pet.setStatus(status);
+    pet.setSyncPoint(syncPoint);
+    pet.setVersion(version);
     return pet;
-  }
-
-  static Category createCategory(long id, String name) {
-    Category category = new Category();
-    category.setId(id);
-    category.setName(name);
-    return category;
   }
 }
--- scray-querying/modules/scray-sync-service-rest/src/main/java/io/swagger/sample/resource/PetResource.java
+++ scray-querying/modules/scray-sync-service-rest/src/main/java/io/swagger/sample/resource/PetResource.java
@@ -22,7 +22,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.sample.data.PetData;
-import io.swagger.sample.model.Pet;
+import io.swagger.sample.model.SyncObject;
 
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
@@ -35,35 +35,33 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/pet")
+@Path("/sync")
 @Produces({"application/json", "application/xml"})
 public class PetResource {
   static PetData petData = new PetData();
 
   @GET
-  @Path("/{petId}")
-  @Operation(summary = "Find pet by ID",
-    tags = {"pets"},
-    description = "Returns a pet when 0 < ID <= 10.  ID > 10 or nonintegers will simulate API error conditions",
+  @Path("/{syncObectId}")
+  @Operation(summary = "Find sync object by ID",
+    tags = {"sync"},
+    description = "Returns the current sync point for the given ID",
     responses = {
-            @ApiResponse(description = "The pet", content = @Content(
-                    schema = @Schema(implementation = Pet.class)
+            @ApiResponse(description = "The sync object", content = @Content(
+                    schema = @Schema(implementation = SyncObject.class)
             )),
             @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
-            @ApiResponse(responseCode = "404", description = "Pet not found")
+            @ApiResponse(responseCode = "404", description = "SyncObject not found")
     })
   public Response getPetById(
       @Parameter(
               description = "ID of pet that needs to be fetched",
               schema = @Schema(
-                      type = "integer",
-                      format = "int64",
-                      description = "param ID of pet that needs to be fetched",
-                      allowableValues = {"1","2","3"}
+                      type = "string",
+                      description = "param ID of pet that needs to be fetched"
               ),
               required = true)
-      @PathParam("petId") Long petId) throws io.swagger.sample.exception.NotFoundException {
-    Pet pet = petData.getPetById(petId);
+      @PathParam("syncObectId") String syncObectId) throws io.swagger.sample.exception.NotFoundException {
+    SyncObject pet = petData.getPetById(syncObectId);
     if (null != pet) {
       return Response.ok().entity(pet).build();
     } else {
@@ -74,72 +72,26 @@ public class PetResource {
   @POST
   @Consumes("application/json")
   @Operation(summary = "Add a new pet to the store",
-    tags = {"pets"},
+    tags = {"sync"},
     responses = {
           @ApiResponse(responseCode = "405", description = "Invalid input")
   })
   public Response addPet(
-      @Parameter(description = "Pet object that needs to be added to the store", required = true) Pet pet) {
+      @Parameter(description = "Pet object that needs to be added to the store", required = true) SyncObject pet) {
     petData.addPet(pet);
     return Response.ok().entity("SUCCESS").build();
   }
 
   @PUT
   @Operation(summary = "Update an existing pet",
-          tags = {"pets"},
+          tags = {"sync"},
           responses = {
                   @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
                   @ApiResponse(responseCode = "404", description = "Pet not found"),
                   @ApiResponse(responseCode = "405", description = "Validation exception") })
   public Response updatePet(
-      @Parameter(description = "Pet object that needs to be added to the store", required = true) Pet pet) {
+      @Parameter(description = "Pet object that needs to be added to the store", required = true) SyncObject pet) {
     petData.addPet(pet);
     return Response.ok().entity("SUCCESS").build();
-  }
-
-  @GET
-  @Path("/findByStatus")
-  @Operation(summary = "Finds Pets by status",
-          tags = {"pets"},
-    description = "Multiple status values can be provided with comma seperated strings",
-          responses = {
-                  @ApiResponse(
-                          content = @Content(mediaType = "application/json",
-                                  schema = @Schema(implementation = Pet.class))),
-                  @ApiResponse(
-                          responseCode = "400", description = "Invalid status value"
-                  )}
-          )
-  public Response findPetsByStatus(
-      @Parameter(
-              description = "Status values that need to be considered for filter",
-              required = true,
-              schema = @Schema(
-                      allowableValues =  {"available","pending","sold"},
-                      defaultValue = "available"
-              )
-      )
-      @QueryParam("status") String status,
-      @BeanParam QueryResultBean qr
-){
-    return Response.ok(petData.findPetByStatus(status)).build();
-  }
-
-  @GET
-  @Path("/findByTags")
-  @Operation(summary = "Finds Pets by tags",
-          tags = {"pets"},
-    description = "Muliple tags can be provided with comma seperated strings. Use tag1, tag2, tag3 for testing.",
-    responses = {
-            @ApiResponse(description = "Pets matching criteria",
-              content = @Content(mediaType = "application/json",
-              schema = @Schema(implementation = Pet.class))
-            ),
-            @ApiResponse(description = "Invalid tag value", responseCode = "400")
-    })
-  @Deprecated
-  public Response findPetsByTags(
-      @Parameter(description = "Tags to filter by", required = true) @QueryParam("tags") String tags) {
-    return Response.ok(petData.findPetByTags(tags)).build();
   }
 }
