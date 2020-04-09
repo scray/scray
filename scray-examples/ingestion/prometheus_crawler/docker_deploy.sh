dc1492ac1448a08a17f8d921706ed3a781b0e37b
Stefan Obermeier <scray@stefan-obermeier.de>
2020-04-09 10:56:18 +0200

    Update docker repo name

--- scray-examples/ingestion/prometheus_crawler/docker_deploy.sh
+++ scray-examples/ingestion/prometheus_crawler/docker_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t scrayorg/examples:prometheus-crawler scray-examples/ingestion/prometheus_crawler/ 
-docker push scrayorg/examples:prometheus-crawler
+docker build -t scrayorg/prometheus-crawler scray-examples/ingestion/prometheus_crawler/ 
+docker push scrayorg/prometheus-crawler
