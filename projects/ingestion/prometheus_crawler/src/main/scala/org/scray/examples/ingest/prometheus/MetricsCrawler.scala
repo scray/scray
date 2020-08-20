e6cd591886f816fec5f9e36fdbd3ecc8dc7aaf38
Stefan Obermeier <st.obermeier@seeburger.de>
2020-08-20 19:51:10 +0200

    Split timerange to 30min ranges

--- projects/ingestion/prometheus_crawler/src/main/scala/org/scray/examples/ingest/prometheus/MetricsCrawler.scala
+++ projects/ingestion/prometheus_crawler/src/main/scala/org/scray/examples/ingest/prometheus/MetricsCrawler.scala
@@ -9,101 +9,113 @@ import scray.hdfs.io.osgi.ReadServiceImpl
 import com.typesafe.scalalogging.Logger
 import org.scray.examples.ingest.prometheus.Client
 
+object MetricsCrawler {
 
-object MetricsCrawler  {
-  
-  def main(args : Array[String]) {
+  def main(args: Array[String]) {
     val client = new Client
-   
+
     val prometheusMetriklistUrl = args(0)
     val destinationPath = args(1)
 
-    var metricsList: List[String] = null 
-    var currentApiRequest = getStartDate(destinationPath + "request_sync.json") 
-    //currentApiRequest = 1568635494
-    var nextApiRequest = 0L
+    val logger = Logger("Prometheus crawler")
 
+    var metricsList: List[String] = null
+    var currentApiRequest = getStartDate(destinationPath + "request_sync.json")
+    var nextApiRequest = 0L
+    val maxStepSize = 30 * 60 // Max timespan for a prometheus query
 
     val hdfsUser = "hdfs"
-    
-    for(i <- 0 to Int.MaxValue) {
-    
+
+    for (i <- 0 to Int.MaxValue) {
+
       // Update metrics list
-      if(i % 10 == 0) {
+      if (i % 10 == 0) {
         metricsList = client.getMetricsList(prometheusMetriklistUrl + "/label/__name__/values")
       }
-      
 
       nextApiRequest = System.currentTimeMillis / 1000
+
+      if (currentApiRequest != 0) {
+        while (Math.abs(nextApiRequest - currentApiRequest) > maxStepSize) {
+
+          logger.info(s"Requested range from ${new Date(currentApiRequest * 1000)} to ${new Date(nextApiRequest * 1000)} exeeds max step size. Query will be splited in chunks of ${maxStepSize} seconds")
+
+          val timerangeEnd = currentApiRequest + maxStepSize
+          queryMetrics(prometheusMetriklistUrl, currentApiRequest, timerangeEnd, metricsList, destinationPath + currentApiRequest + "_")
+          currentApiRequest = timerangeEnd
+        }
+      }
+
       queryMetrics(prometheusMetriklistUrl, currentApiRequest, nextApiRequest, metricsList, destinationPath + currentApiRequest + "_")
       currentApiRequest = nextApiRequest
 
       println("Next api request\t" + nextApiRequest)
-      
+
       perstistLastStartDate(destinationPath + "request_sync.json", nextApiRequest)
-      
+
       // Query every 5 minutes
-      Thread.sleep(60 * 5 *  1000)
+      Thread.sleep(60 * 5 * 1000)
     }
   }
+
   
   def queryMetrics(prometheusMetriklistUrl: String, dataStartDate: Long, dataEndDate: Long, matricsToQuery: List[String], outPath: String) = {
     val logger = Logger("Prometheus crawler")
     val client = new Client
     val writeService = new WriteServiceImpl
-    
+
     val config = new WriteParameter.Builder()
-    .setPath(outPath)
-    .setFileFormat(SequenceKeyValueFormat.SEQUENCEFILE_TEXT_TEXT)
-    .setSequenceFileCompressionType("BLOCK")
-    .setUser("hdfs")
-    .createConfiguration
+      .setPath(outPath)
+      .setFileFormat(SequenceKeyValueFormat.SEQUENCEFILE_TEXT_TEXT)
+      .setSequenceFileCompressionType("BLOCK")
+      .setUser("hdfs")
+      .createConfiguration
 
     val writeId = writeService.createWriter(config)
-    
+
     val numMetricsWritte = matricsToQuery.foldLeft(0)((acc, metric) => {
 
       val nextMetricQuery = prometheusMetriklistUrl + "/query_range?query=" + metric + "&start=" + dataStartDate + "&end=" + dataEndDate + "&step=30s"
       val metrics = client.getExtractedMetricValues(nextMetricQuery)
-      
-      logger.info("Query metric: " +  nextMetricQuery)
-      
+
+      logger.debug("Query metric: " + nextMetricQuery)
+
       val numMetricsWritte = metrics.foldLeft(0)((acc, metric) => {
-        writeService.insert(writeId, dataStartDate.toString(), System.currentTimeMillis, metric.getBytes)    
+        writeService.insert(writeId, dataStartDate.toString(), System.currentTimeMillis, metric.getBytes)
         acc + 1
       })
-      
-      acc + numMetricsWritte 
+
+      acc + numMetricsWritte
     })
-    
+
     writeService.close(writeId)
     println(new Date + " num metrics written " + numMetricsWritte)
   }
-  
+
   def getStartDate(storragePath: String): Long = {
     val versionReader = new FileVersionedDataApiImpl
     val readService = new ReadServiceImpl
-    
+
     try {
       val inStream = readService.getInputStream(storragePath, "hdfs", "".getBytes).get
       versionReader.load(inStream)
     } catch {
       case e: Throwable => println("Unable to read verion file. Use default value" + e.getMessage)
     }
-    
+
     val latestVersion = versionReader.getLatestVersion("http://org.scray.examples/resource/prometheus-crawler/version", "_") match {
-      case None => System.currentTimeMillis / 1000
+      case None             => System.currentTimeMillis / 1000
       case Some(oldVersion) => oldVersion.data.toLong
     }
-    
+
     latestVersion
   }
-  
+
   def perstistLastStartDate(destinationPath: String, date: Long) {
     val versionWriter = new FileVersionedDataApiImpl
-    
+
     versionWriter.updateVersion("http://org.scray.examples/resource/prometheus-crawler/version", "_", System.currentTimeMillis(), date.toString)
-    
+
     try {
       val outStream = new WriteServiceImpl().writeRawFile(destinationPath, "hdfs", "".getBytes)
       versionWriter.persist(outStream)
