a0e95ac280c637c6b776efc10a6a5d19af805647
Stefan Obermeier <st.obermeier@seeburger.de>
2020-11-03 17:28:46 +0100

    Update hostname

--- projects/invoice-hyperledger-fabric/containers/hl-fabric-peer-crypto-generator/docker_deploy.sh
+++ projects/invoice-hyperledger-fabric/containers/hl-fabric-peer-crypto-generator/docker_deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_TOKEN"
+echo "$DOCKER_USERNAME"
 docker build -t scrayorg/hl-fabric-peer-crypto-generator . 
 docker push scrayorg/hl-fabric-peer-crypto-generator 
