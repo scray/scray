91a11533146db9a92be2e73bb7c24c32e2c31f9f
Stefan Obermeier <st.obermeier@seeburger.de>
2020-11-18 01:53:55 +0100

    Add missing base path

--- projects/invoice-hyperledger-fabric/scripts/example_network_commit_cc.sh
+++ projects/invoice-hyperledger-fabric/scripts/example_network_commit_cc.sh
@@ -3,6 +3,13 @@ HOST_IP=$2
 
 export PKGID=basic_1.0:5a294a12a1a89cd4eed3d4234fbc79f42eab2ac20cd176bc8ebbc07c597cd0ee
 
+CERT_BASE_PATH=/opt/gopath/src/github.com/hyperledger/fabric/peer
+
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${CERT_BASE_PATH}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${CERT_BASE_PATH}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${CERT_BASE_PATH}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
 export CORE_PEER_MSPCONFIGPATH=${CERT_BASE_PATH}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
