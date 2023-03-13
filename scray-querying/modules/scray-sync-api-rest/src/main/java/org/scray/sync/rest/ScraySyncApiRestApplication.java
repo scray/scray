42ca98a1feec2d6893485d51d0176ba7b8fe35ca
Stefan Obermeier <st.obermeier@seeburger.de>
2023-03-13 10:51:18 +0100

    Add generated api code base

---
+++ scray-querying/modules/scray-sync-api-rest/src/main/java/org/scray/sync/rest/ScraySyncApiRestApplication.java
@@ -0,0 +1,13 @@
+package org.scray.sync.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraySyncApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraySyncApiRestApplication.class, args);
+	}
+
+}
