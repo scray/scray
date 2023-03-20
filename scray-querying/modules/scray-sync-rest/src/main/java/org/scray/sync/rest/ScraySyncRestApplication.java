643ed6228e8353fc196cd5c65f8b729b2344d5fb
Stefan Obermeier <st.obermeier@seeburger.de>
2023-03-20 17:30:22 +0100

    Add raw rest api

---
+++ scray-querying/modules/scray-sync-rest/src/main/java/org/scray/sync/rest/ScraySyncRestApplication.java
@@ -0,0 +1,13 @@
+package org.scray.sync.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraySyncRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraySyncRestApplication.class, args);
+	}
+
+}
---
+++ scray-querying/modules/scray-sync-rest/src/test/java/org/scray/sync/rest/ScraySyncRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scray.sync.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraySyncRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
