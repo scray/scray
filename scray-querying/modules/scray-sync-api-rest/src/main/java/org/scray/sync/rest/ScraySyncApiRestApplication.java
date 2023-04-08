9a40ef68414a1b800eabacbdfa62119b928b07a3
Stefan Obermeier <st.obermeier@seeburger.de>
2023-04-08 19:16:04 +0200

    Clean up

--- scray-querying/modules/scray-sync-api-rest/src/main/java/org/scray/sync/rest/ScraySyncApiRestApplication.java
+++ scray-querying/modules/scray-sync-api-rest/src/main/java/org/scray/sync/rest/ScraySyncApiRestApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		info = @Info(title = "Scray-Sync API", version = "1.0.0"
 				, description = "This a REST-API to manage versions of data",
 				license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")),
-		servers = {@Server(url = "http://localhost:8082")},
+		servers = {@Server(url = "http://ml-integration.research.dev.seeburger.de:8082"), @Server(url = "http://localhost:8082")},
 		tags = {@Tag(name = "Sync-API", description = "Resources to query versions of data")}
 )
 public class ScraySyncApiRestApplication {
