766ed51224d3107d92924e2ee4ec25bed268bdc4
Stefan Obermeier <st.obermeier@seeburger.de>
2023-05-22 16:38:03 +0200

    Migrate cassandra sync impl to scla 2.13

--- scray-querying/modules/scray-sync-cassandra/src/main/scala/scray/sync/VersionedDataReader.scala
+++ scray-querying/modules/scray-sync-cassandra/src/main/scala/scray/sync/VersionedDataReader.scala
@@ -3,10 +3,9 @@ package scray.sync
 import scala.util.Try
 import scray.sync.api.VersionedData
 import scala.util.Failure
-import scala.collection.mutable.LinkedList
 
 class VersionedDataReader {
   def get(): Try[Iterator[VersionedData]] = {
-    Try((new LinkedList[VersionedData]).iterator)
+    Try((List.empty[VersionedData].iterator))
   }
 }
