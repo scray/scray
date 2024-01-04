168e6d01df099d7e582312d05cf7f577d95e933b
Stefan Obermeier <st.obermeier@seeburger.de>
2024-01-04 11:07:00 +0100

    Add configuration reader and writer

---
+++ scray-examples/ml-job-coordination/agent/ki-side/agent/src/main/java/org/scray/integration/ai/agent/conf/ConfigurationLoader.java
@@ -0,0 +1,69 @@
+package org.scray.integration.ai.agent.conf;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.MessageFormat;
+
+import org.apache.logging.log4j.message.Message;
+import org.scray.integration.ai.agent.AiIntegrationAgent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConfigurationLoader {
+
+	private String configurationFileLocation = System.getenv("SCRAY_K8S_AGENT_CONF");
+	private static final Logger logger = LoggerFactory.getLogger(ConfigurationLoader.class);
+
+
+	public ConfigurationLoader() {
+		if(configurationFileLocation == null) {
+			this.configurationFileLocation = "conf/k8s-agent.conf";
+		}
+	}
+
+	private void write(Environments envs) throws ConfiguratonIoError{
+		FileOutputStream outStream;
+		try {
+			outStream = new FileOutputStream(configurationFileLocation);
+			this.write(envs, outStream);
+		} catch (IOException e) {
+			logger.debug(MessageFormat.format("Error while writing configuration {0}", e));
+			e.printStackTrace();
+			throw new ConfiguratonIoError(e.getMessage());
+		}
+
+	}
+
+	public void write(Environments envs, OutputStream stream) throws ConfiguratonIoError, StreamWriteException, DatabindException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.writeValue(stream, envs);
+	}
+
+	public Environments readConfiguration() throws ConfiguratonIoError {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Environments envs;
+		try {
+			envs = objectMapper.readValue(this.configurationFileLocation, Environments.class);
+		} catch (JsonProcessingException e) {
+			logger.debug(MessageFormat.format("Error while reading configuration {0}", e));
+			e.printStackTrace();
+			throw new ConfiguratonIoError(e.getMessage());
+		}
+		return envs;
+	}
+
+	public class ConfiguratonIoError extends Exception {
+	    public ConfiguratonIoError(String errorMessage) {
+	        super(errorMessage);
+	    }
+	}
+}
--- scray-examples/ml-job-coordination/agent/ki-side/agent/src/main/java/org/scray/integration/ai/agent/conf/Environment.java
+++ scray-examples/ml-job-coordination/agent/ki-side/agent/src/main/java/org/scray/integration/ai/agent/conf/Environment.java
@@ -3,7 +3,7 @@ package org.scray.integration.ai.agent.conf;
 public class Environment{
     public String name;
     public String id;
-    public String ingress;
-    public String k8sJobDescripton;
-    public String k8sDeploymentDescription;
+    public String ingressTemplate;
+    public String k8sJobDescriptonTemplate;
+    public String k8sDeploymentDescriptionTemplate;
 }
