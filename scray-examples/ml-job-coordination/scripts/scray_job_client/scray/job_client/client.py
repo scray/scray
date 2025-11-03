8532454853a7a0ffee31cc14b750577e7ee9cb5d
Stefan Obermeier <st.obermeier@seeburger.de>
2025-11-03 09:08:35 +0100

    Add method to get out data

--- scray-examples/ml-job-coordination/scripts/scray_job_client/scray/job_client/client.py
+++ scray-examples/ml-job-coordination/scripts/scray_job_client/scray/job_client/client.py
@@ -333,3 +333,49 @@ class ScrayJobClient:
                 os.remove(temp_tar_path)
                 print(f"Removed temporary file {temp_tar_path}")
 
+    def get_job_out_data(self, job_name, destination_path, data_integration_user, data_integration_host):
+        """
+        Downloads the files stored in the output folder of the job
+
+        :param job_name: Name of the job.
+        :param destination_path: Path where the extracted files should be stored.
+        """
+
+        import os
+        temp_dir = tempfile.gettempdir()
+        temp_tar_path = os.path.join(temp_dir, f"{job_name}.tar.gz")
+        
+        # Ensure the destination path exists
+        os.makedirs(destination_path, exist_ok=True)
+
+        transport = paramiko.Transport((data_integration_host, 22))
+        
+        try:
+            private_key_path = f"{Path.home()}/.ssh/id_rsa"
+            key = paramiko.RSAKey.from_private_key_file(private_key_path)
+
+            # Connect to SFTP
+            transport.connect(username=data_integration_user, pkey=key)
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            
+            # Download the archive
+            remote_path = f"sftp-share/{job_name}_out.tar.gz"
+            print(f"Downloading {remote_path} to {temp_tar_path}")
+            sftp.get(remote_path, temp_tar_path)
+            sftp.close()
+
+            # Extract the archive
+            with tarfile.open(temp_tar_path, "r:gz") as tar:
+                tar.extractall(path=destination_path)
+                print(f"Extracted {job_name}_out.tar.gz to {destination_path}")
+
+        except Exception as e:
+            print(f"Error during download and extraction: {e}")
+        finally:
+            transport.close()
+            
+            # Remove the downloaded archive after extraction
+            if os.path.exists(temp_tar_path):
+                os.remove(temp_tar_path)
+                print(f"Removed temporary file {temp_tar_path}")
+
